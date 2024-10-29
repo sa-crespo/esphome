567bb16ea84322203ccd3f44631ecae780d1bdfb
sa-crespo <sa_crespo@hotmail.com>
2024-10-29 05:45:51 +0000

    Binary Sensor Multi

---
+++ esphome/components/binary_sensor_multi/binary_sensor_multi.cpp
@@ -0,0 +1,37 @@
+#include "binary_sensor_multi.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace binary_sensor_multi {
+
+static const char *const TAG = "binary_sensor_multi";
+
+void BinarySensorMulti::dump_config() { LOG_BINARY_SENSOR("", "Multi Binary Sensor", this); }
+
+void BinarySensorMulti::loop() { this->process_sensors_(); }
+
+void BinarySensorMulti::process_sensors_() {
+  uint8_t num_active_sensors = 0;
+
+  // - check all binary_sensors for its state
+  //  - if active, add its value to total_current_value.
+  // - creates a bitmask for the binary_sensor states on all channels
+  for (size_t i = 0; i < this->channels_.size(); i++) {
+    auto bs = this->channels_[i];
+    if (bs.binary_sensor->state) {
+      num_active_sensors++;
+    }
+  }
+
+  this->publish_state((num_active_sensors > 0));
+}
+
+void BinarySensorMulti::add_channel(binary_sensor::BinarySensor *sensor) {
+  BinarySensorMultiGpioChannel sensor_channel{
+      .binary_sensor = sensor,
+  };
+  this->channels_.push_back(sensor_channel);
+}
+
+}  // namespace binary_sensor_multi
+}  // namespace esphome
---
+++ esphome/components/binary_sensor_multi/binary_sensor_multi.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/sensor/sensor.h"
+
+#include <vector>
+
+namespace esphome {
+namespace binary_sensor_multi {
+
+struct BinarySensorMultiChannel {
+  binary_sensor::BinarySensor *binary_sensor;
+};
+
+/** Class to map one or more binary_sensors to one Sensor.
+ *
+ * Each binary sensor has configured parameters that each mapping type uses to compute the single numerical result
+ */
+class BinarySensorMulti : public binary_sensor::BinarySensor, public Component {
+ public:
+  void dump_config() override;
+
+  /**
+   * The loop calls the configured type processing method
+   *
+   * The processing method loops through all sensors and calculates the numerical result
+   * The result is only published if a binary sensor state has changed or, for some types, on initial boot
+   */
+  void loop() override;
+
+  /**
+   * Add binary_sensors to the group when only one parameter is needed for the configured mapping type.
+   *
+   * @param *sensor The binary sensor.
+   * @param value  The value this binary_sensor represents
+   */
+  void add_channel(binary_sensor::BinarySensor *sensor);
+
+ protected:
+  std::vector<BinarySensorMultiGpioChannel> channels_{};
+  void process_sensors_();
+};
+
+}  // namespace binary_sensor_multi
+}  // namespace esphome

34562e05b1b558e0883bcd2f5e5498e62916458b
J. Nick Koston <nick@koston.org>
2025-05-14 21:25:44 -0500

    Avoid protobuf message construction when tx buffer is full (#8787)
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/api/api_connection.h
+++ esphome/components/api/api_connection.h
@@ -9,14 +9,12 @@
 #include "esphome/core/application.h"
 #include "esphome/core/component.h"
 #include "esphome/core/entity_base.h"
+#include "esphome/core/entity_base.h"
 
 #include <vector>
 
 namespace esphome {
 namespace api {
-
-// Keepalive timeout in milliseconds
-static constexpr uint32_t KEEPALIVE_TIMEOUT_MS = 60000;
 
 using send_message_t = bool (APIConnection::*)(void *);
 
@@ -35,8 +33,10 @@ class DeferredMessageQueue {
    protected:
     void *source_;
     send_message_t send_message_;
+    send_message_t send_message_;
 
    public:
+    DeferredMessage(void *source, send_message_t send_message) : source_(source), send_message_(send_message) {}
     DeferredMessage(void *source, send_message_t send_message) : source_(source), send_message_(send_message) {}
     bool operator==(const DeferredMessage &test) const {
       return (source_ == test.source_ && send_message_ == test.send_message_);
@@ -51,10 +51,13 @@ class DeferredMessageQueue {
 
   // helper for allowing only unique entries in the queue
   void dmq_push_back_with_dedup_(void *source, send_message_t send_message);
+  void dmq_push_back_with_dedup_(void *source, send_message_t send_message);
 
  public:
   DeferredMessageQueue(APIConnection *api_connection) : api_connection_(api_connection) {}
   void process_queue();
+  void defer(void *source, send_message_t send_message);
+  bool empty() const { return deferred_queue_.empty(); }
   void defer(void *source, send_message_t send_message);
   bool empty() const { return deferred_queue_.empty(); }
 };
@@ -81,12 +84,23 @@ class APIConnection : public APIServerConnection {
   bool try_send_binary_sensor_info_(binary_sensor::BinarySensor *binary_sensor);
 
  public:
+ protected:
+  bool try_send_binary_sensor_state_(binary_sensor::BinarySensor *binary_sensor);
+  bool try_send_binary_sensor_state_(binary_sensor::BinarySensor *binary_sensor, bool state);
+  bool try_send_binary_sensor_info_(binary_sensor::BinarySensor *binary_sensor);
+
+ public:
 #endif
 #ifdef USE_COVER
   bool send_cover_state(cover::Cover *cover);
   void send_cover_info(cover::Cover *cover);
   void cover_command(const CoverCommandRequest &msg) override;
 
+ protected:
+  bool try_send_cover_state_(cover::Cover *cover);
+  bool try_send_cover_info_(cover::Cover *cover);
+
+ public:
  protected:
   bool try_send_cover_state_(cover::Cover *cover);
   bool try_send_cover_info_(cover::Cover *cover);
@@ -103,6 +117,11 @@ class APIConnection : public APIServerConnection {
   bool try_send_fan_info_(fan::Fan *fan);
 
  public:
+ protected:
+  bool try_send_fan_state_(fan::Fan *fan);
+  bool try_send_fan_info_(fan::Fan *fan);
+
+ public:
 #endif
 #ifdef USE_LIGHT
   bool send_light_state(light::LightState *light);
@@ -114,11 +133,22 @@ class APIConnection : public APIServerConnection {
   bool try_send_light_info_(light::LightState *light);
 
  public:
+ protected:
+  bool try_send_light_state_(light::LightState *light);
+  bool try_send_light_info_(light::LightState *light);
+
+ public:
 #endif
 #ifdef USE_SENSOR
   bool send_sensor_state(sensor::Sensor *sensor, float state);
   void send_sensor_info(sensor::Sensor *sensor);
 
+ protected:
+  bool try_send_sensor_state_(sensor::Sensor *sensor);
+  bool try_send_sensor_state_(sensor::Sensor *sensor, float state);
+  bool try_send_sensor_info_(sensor::Sensor *sensor);
+
+ public:
  protected:
   bool try_send_sensor_state_(sensor::Sensor *sensor);
   bool try_send_sensor_state_(sensor::Sensor *sensor, float state);
@@ -137,11 +167,23 @@ class APIConnection : public APIServerConnection {
   bool try_send_switch_info_(switch_::Switch *a_switch);
 
  public:
+ protected:
+  bool try_send_switch_state_(switch_::Switch *a_switch);
+  bool try_send_switch_state_(switch_::Switch *a_switch, bool state);
+  bool try_send_switch_info_(switch_::Switch *a_switch);
+
+ public:
 #endif
 #ifdef USE_TEXT_SENSOR
   bool send_text_sensor_state(text_sensor::TextSensor *text_sensor, std::string state);
   void send_text_sensor_info(text_sensor::TextSensor *text_sensor);
 
+ protected:
+  bool try_send_text_sensor_state_(text_sensor::TextSensor *text_sensor);
+  bool try_send_text_sensor_state_(text_sensor::TextSensor *text_sensor, std::string state);
+  bool try_send_text_sensor_info_(text_sensor::TextSensor *text_sensor);
+
+ public:
  protected:
   bool try_send_text_sensor_state_(text_sensor::TextSensor *text_sensor);
   bool try_send_text_sensor_state_(text_sensor::TextSensor *text_sensor, std::string state);
@@ -158,12 +200,21 @@ class APIConnection : public APIServerConnection {
   bool try_send_camera_info_(esp32_camera::ESP32Camera *camera);
 
  public:
+ protected:
+  bool try_send_camera_info_(esp32_camera::ESP32Camera *camera);
+
+ public:
 #endif
 #ifdef USE_CLIMATE
   bool send_climate_state(climate::Climate *climate);
   void send_climate_info(climate::Climate *climate);
   void climate_command(const ClimateCommandRequest &msg) override;
 
+ protected:
+  bool try_send_climate_state_(climate::Climate *climate);
+  bool try_send_climate_info_(climate::Climate *climate);
+
+ public:
  protected:
   bool try_send_climate_state_(climate::Climate *climate);
   bool try_send_climate_info_(climate::Climate *climate);
@@ -181,12 +232,23 @@ class APIConnection : public APIServerConnection {
   bool try_send_number_info_(number::Number *number);
 
  public:
+ protected:
+  bool try_send_number_state_(number::Number *number);
+  bool try_send_number_state_(number::Number *number, float state);
+  bool try_send_number_info_(number::Number *number);
+
+ public:
 #endif
 #ifdef USE_DATETIME_DATE
   bool send_date_state(datetime::DateEntity *date);
   void send_date_info(datetime::DateEntity *date);
   void date_command(const DateCommandRequest &msg) override;
 
+ protected:
+  bool try_send_date_state_(datetime::DateEntity *date);
+  bool try_send_date_info_(datetime::DateEntity *date);
+
+ public:
  protected:
   bool try_send_date_state_(datetime::DateEntity *date);
   bool try_send_date_info_(datetime::DateEntity *date);
@@ -203,6 +265,11 @@ class APIConnection : public APIServerConnection {
   bool try_send_time_info_(datetime::TimeEntity *time);
 
  public:
+ protected:
+  bool try_send_time_state_(datetime::TimeEntity *time);
+  bool try_send_time_info_(datetime::TimeEntity *time);
+
+ public:
 #endif
 #ifdef USE_DATETIME_DATETIME
   bool send_datetime_state(datetime::DateTimeEntity *datetime);
@@ -214,12 +281,23 @@ class APIConnection : public APIServerConnection {
   bool try_send_datetime_info_(datetime::DateTimeEntity *datetime);
 
  public:
+ protected:
+  bool try_send_datetime_state_(datetime::DateTimeEntity *datetime);
+  bool try_send_datetime_info_(datetime::DateTimeEntity *datetime);
+
+ public:
 #endif
 #ifdef USE_TEXT
   bool send_text_state(text::Text *text, std::string state);
   void send_text_info(text::Text *text);
   void text_command(const TextCommandRequest &msg) override;
 
+ protected:
+  bool try_send_text_state_(text::Text *text);
+  bool try_send_text_state_(text::Text *text, std::string state);
+  bool try_send_text_info_(text::Text *text);
+
+ public:
  protected:
   bool try_send_text_state_(text::Text *text);
   bool try_send_text_state_(text::Text *text, std::string state);
@@ -238,11 +316,21 @@ class APIConnection : public APIServerConnection {
   bool try_send_select_info_(select::Select *select);
 
  public:
+ protected:
+  bool try_send_select_state_(select::Select *select);
+  bool try_send_select_state_(select::Select *select, std::string state);
+  bool try_send_select_info_(select::Select *select);
+
+ public:
 #endif
 #ifdef USE_BUTTON
   void send_button_info(button::Button *button);
   void button_command(const ButtonCommandRequest &msg) override;
 
+ protected:
+  bool try_send_button_info_(button::Button *button);
+
+ public:
  protected:
   bool try_send_button_info_(button::Button *button);
 
@@ -253,6 +341,12 @@ class APIConnection : public APIServerConnection {
   void send_lock_info(lock::Lock *a_lock);
   void lock_command(const LockCommandRequest &msg) override;
 
+ protected:
+  bool try_send_lock_state_(lock::Lock *a_lock);
+  bool try_send_lock_state_(lock::Lock *a_lock, lock::LockState state);
+  bool try_send_lock_info_(lock::Lock *a_lock);
+
+ public:
  protected:
   bool try_send_lock_state_(lock::Lock *a_lock);
   bool try_send_lock_state_(lock::Lock *a_lock, lock::LockState state);
@@ -270,12 +364,22 @@ class APIConnection : public APIServerConnection {
   bool try_send_valve_info_(valve::Valve *valve);
 
  public:
+ protected:
+  bool try_send_valve_state_(valve::Valve *valve);
+  bool try_send_valve_info_(valve::Valve *valve);
+
+ public:
 #endif
 #ifdef USE_MEDIA_PLAYER
   bool send_media_player_state(media_player::MediaPlayer *media_player);
   void send_media_player_info(media_player::MediaPlayer *media_player);
   void media_player_command(const MediaPlayerCommandRequest &msg) override;
 
+ protected:
+  bool try_send_media_player_state_(media_player::MediaPlayer *media_player);
+  bool try_send_media_player_info_(media_player::MediaPlayer *media_player);
+
+ public:
  protected:
   bool try_send_media_player_state_(media_player::MediaPlayer *media_player);
   bool try_send_media_player_info_(media_player::MediaPlayer *media_player);
@@ -334,12 +438,23 @@ class APIConnection : public APIServerConnection {
   bool try_send_alarm_control_panel_info_(alarm_control_panel::AlarmControlPanel *a_alarm_control_panel);
 
  public:
+ protected:
+  bool try_send_alarm_control_panel_state_(alarm_control_panel::AlarmControlPanel *a_alarm_control_panel);
+  bool try_send_alarm_control_panel_info_(alarm_control_panel::AlarmControlPanel *a_alarm_control_panel);
+
+ public:
 #endif
 
 #ifdef USE_EVENT
   void send_event(event::Event *event, std::string event_type);
   void send_event_info(event::Event *event);
 
+ protected:
+  bool try_send_event_(event::Event *event);
+  bool try_send_event_(event::Event *event, std::string event_type);
+  bool try_send_event_info_(event::Event *event);
+
+ public:
  protected:
   bool try_send_event_(event::Event *event);
   bool try_send_event_(event::Event *event, std::string event_type);
@@ -353,6 +468,11 @@ class APIConnection : public APIServerConnection {
   void send_update_info(update::UpdateEntity *update);
   void update_command(const UpdateCommandRequest &msg) override;
 
+ protected:
+  bool try_send_update_state_(update::UpdateEntity *update);
+  bool try_send_update_info_(update::UpdateEntity *update);
+
+ public:
  protected:
   bool try_send_update_state_(update::UpdateEntity *update);
   bool try_send_update_info_(update::UpdateEntity *update);
@@ -419,12 +539,106 @@ class APIConnection : public APIServerConnection {
     return {&this->proto_write_buffer_};
   }
   bool try_to_clear_buffer(bool log_out_of_space);
+  bool try_to_clear_buffer(bool log_out_of_space);
   bool send_buffer(ProtoWriteBuffer buffer, uint32_t message_type) override;
 
   std::string get_client_combined_info() const { return this->client_combined_info_; }
 
  protected:
   friend APIServer;
+
+  /**
+   * Generic send entity state method to reduce code duplication.
+   * Only attempts to build and send the message if the transmit buffer is available.
+   *
+   * This is the base version for entities that use their current state.
+   *
+   * @param entity The entity to send state for
+   * @param try_send_func The function that tries to send the state
+   * @return True on success or message deferred, false if subscription check failed
+   */
+  bool send_state_(esphome::EntityBase *entity, send_message_t try_send_func) {
+    if (!this->state_subscription_)
+      return false;
+    if (this->try_to_clear_buffer(true) && (this->*try_send_func)(entity)) {
+      return true;
+    }
+    this->deferred_message_queue_.defer(entity, try_send_func);
+    return true;
+  }
+
+  /**
+   * Send entity state method that handles explicit state values.
+   * Only attempts to build and send the message if the transmit buffer is available.
+   *
+   * This method accepts a state parameter to be used instead of the entity's current state.
+   * It attempts to send the state with the provided value first, and if that fails due to buffer constraints,
+   * it defers the entity for later processing using the entity-only function.
+   *
+   * @tparam EntityT The entity type
+   * @tparam StateT Type of the state parameter
+   * @tparam Args Additional argument types (if any)
+   * @param entity The entity to send state for
+   * @param try_send_entity_func The function that tries to send the state with entity pointer only
+   * @param try_send_state_func The function that tries to send the state with entity and state parameters
+   * @param state The state value to send
+   * @param args Additional arguments to pass to the try_send_state_func
+   * @return True on success or message deferred, false if subscription check failed
+   */
+  template<typename EntityT, typename StateT, typename... Args>
+  bool send_state_with_value_(EntityT *entity, bool (APIConnection::*try_send_entity_func)(EntityT *),
+                              bool (APIConnection::*try_send_state_func)(EntityT *, StateT, Args...), StateT state,
+                              Args... args) {
+    if (!this->state_subscription_)
+      return false;
+    if (this->try_to_clear_buffer(true) && (this->*try_send_state_func)(entity, state, args...)) {
+      return true;
+    }
+    this->deferred_message_queue_.defer(entity, reinterpret_cast<send_message_t>(try_send_entity_func));
+    return true;
+  }
+
+  /**
+   * Generic send entity info method to reduce code duplication.
+   * Only attempts to build and send the message if the transmit buffer is available.
+   *
+   * @param entity The entity to send info for
+   * @param try_send_func The function that tries to send the info
+   */
+  void send_info_(esphome::EntityBase *entity, send_message_t try_send_func) {
+    if (this->try_to_clear_buffer(true) && (this->*try_send_func)(entity)) {
+      return;
+    }
+    this->deferred_message_queue_.defer(entity, try_send_func);
+  }
+
+  /**
+   * Generic function for generating entity info response messages.
+   * This is used to reduce duplication in the try_send_*_info functions.
+   *
+   * @param entity The entity to generate info for
+   * @param response The response object
+   * @param send_response_func Function pointer to send the response
+   * @return True if the message was sent successfully
+   */
+  template<typename ResponseT>
+  bool try_send_entity_info_(esphome::EntityBase *entity, ResponseT &response,
+                             bool (APIServerConnectionBase::*send_response_func)(const ResponseT &)) {
+    // Set common fields that are shared by all entity types
+    response.key = entity->get_object_id_hash();
+    response.object_id = entity->get_object_id();
+
+    if (entity->has_own_name())
+      response.name = entity->get_name();
+
+    // Set common EntityBase properties
+    response.icon = entity->get_icon();
+    response.disabled_by_default = entity->is_disabled_by_default();
+    response.entity_category = static_cast<enums::EntityCategory>(entity->get_entity_category());
+
+    // Send the response using the provided send method
+    return (this->*send_response_func)(response);
+  }
 
   /**
    * Generic send entity state method to reduce code duplication.
